7259e021baada4f804146aff7f39a3690b8f9e74
Jason Emerick <jason@mobelux.com>
2013-05-07 13:36:21 -0400

    add pyres_schedule command and SCHEDULUER_PIDFILE setting

--- django_pyres/conf.py
+++ django_pyres/conf.py
@@ -1,16 +1,17 @@
 from django.conf import settings #noqa
 from appconf import AppConf
 
+
 class PyresConf(AppConf):
     HOST = "localhost:6379"
     PASSWORD = None
-    USE_QUEUE=True
+    USE_QUEUE = True
     WORKER_PIDFILE = None
     WORKER_INTERVAL = None
     WORKER_LOGFILE = None
-    WORKER_TIMEOUT= None
-    MANAGER_POOL_SIZE=2
-
+    WORKER_TIMEOUT = None
+    MANAGER_POOL_SIZE = 2
+    SCHEDULUER_PIDFILE = None
 
     class Meta:
         prefix = 'pyres'
